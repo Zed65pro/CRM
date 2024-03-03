198a2b6ebc1e544132a1ff1edd662f9568d18f62
Zaid Ibaisi <zed65pro@hotmail.com>
2024-03-03 15:22:18 +0400

    - new font

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,8 +35,14 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #f4f4f4;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto Slab", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
