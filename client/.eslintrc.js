a0f74cb44b4ae47890dbf55f4e39f68f0883a116
Zaid Ibaisi <zed65pro@hotmail.com>
2024-03-03 22:14:57 +0400

    eslint stuff

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,8 +15,8 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": "off",
+    "requireConfigFile": false,
     "vue/no-mutating-props": "off",
     "vue/multi-word-component-names": "off",
-    "requireConfigFile": false,
   },
 };
