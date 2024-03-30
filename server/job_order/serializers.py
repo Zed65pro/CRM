c1d17343134e097f3393ce37b33d2c03c51554d6
Zaid Ibaisi <zed65pro@hotmail.com>
2024-03-31 00:18:43 +0300

    fix serializer

--- server/job_order/serializers.py
+++ server/job_order/serializers.py
@@ -5,10 +5,12 @@ from .models import JobOrder, JobOrderImage
 
 class JobOrderSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
+    optional_fields = ['location','status']
 
     class Meta:
         model = JobOrder
         fields = '__all__'
+        read_only_fields = ['created_at', 'updated_at','created_by']
 
 class JobOrderImageSerializer(serializers.ModelSerializer):
     uploaded_by = UserSerializer(read_only=True)
@@ -17,3 +19,4 @@ class JobOrderImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobOrderImage
         fields = ['__all__']
+        read_only_fields = ['created_at', 'updated_at','uploaded_by','job_order']
